3176268b1fc6ac8e41b24898eb0901afcefc62ea
Jmart79 <justinjustinjust111@gmail.com>
2020-12-29 20:03:18 -0700

    Base Classes

---
+++ Blackjack/Blackjack/Models/Card.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blackjack.Models
+{
+    public class Card
+    {
+    }
+}
---
+++ Blackjack/Blackjack/Pages/Game/GameBoard.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Blackjack.Pages.Game
+{
+    public class GameBoardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
