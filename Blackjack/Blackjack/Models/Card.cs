9b986b463b24effe42f5f8a14bbae0b8a3d1b413
Jmart79 <justinjustinjust111@gmail.com>
2020-12-29 21:56:51 -0700

    Images / Part of Deck Logic

--- Blackjack/Blackjack/Models/Card.cs
+++ Blackjack/Blackjack/Models/Card.cs
@@ -8,6 +8,13 @@ namespace Blackjack.Models
 {
     public class Card
     {
-      
+        public string CardName { get; set; }
+        public string CardImageName { get; set; }
+
+        public Card(string cardName, string cardImageName)
+        {
+            this.CardName = cardName;
+            this.CardImageName = cardImageName;
+        }
     }
 }
---
+++ Blackjack/Blackjack/Models/Deck.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blackjack.Models
+{
+    public class Deck
+    {
+        private Card[] CardStack { get; set; }
+        
+        public Deck()
+        {
+            CardStack = ShuffleCards(GenerateCards());
+
+        }
+
+        public Deck(Card[] cardStack)
+        {
+            if(cardStack.Length == 52)
+            {
+                 this.CardStack = cardStack;
+            }
+        }
+
+        private Card[] ShuffleCards(Card[] initialDeck)
+        {
+            return null;
+        }
+
+        private Card[] GenerateCards()
+        {
+            string[] fileEntries = Directory.GetFiles("~/Images/CardFronts");
+            List<Card> generatedCardList = new List<Card>();
+            Card placeholderCard;
+            foreach(string fileEntry in fileEntries)
+            {
+                string[] fileNameContents = fileEntry.Split(".");
+                placeholderCard = new Card(fileNameContents[0], fileEntry);
+                generatedCardList.Add(placeholderCard);
+            }
+
+            return generatedCardList.ToArray();
+        }
+    }
+}
