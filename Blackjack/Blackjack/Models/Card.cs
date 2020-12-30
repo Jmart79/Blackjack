15e66512d8a523512fab6029779a39ed38489242
Jmart79 <justinjustinjust111@gmail.com>
2020-12-29 20:06:27 -0700

    CardSuit Enum

--- Blackjack/Blackjack/Models/Card.cs
+++ Blackjack/Blackjack/Models/Card.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Blackjack.Models.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,6 @@ namespace Blackjack.Models
 {
     public class Card
     {
+      
     }
 }
